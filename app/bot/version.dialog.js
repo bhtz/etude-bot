3da9bf16f58bab6cb00654e1a9e68e3bd6bb7c13
HEINTZ Benjamin <benjamin.heintz@soprasteria.com>
2017-04-11 15:20:34 +0200

    default

--- app/bot/version.dialog.js
+++ app/bot/version.dialog.js
@@ -9,9 +9,14 @@ module.exports = class VersionDialog {
 
         this.bot.dialog('/', this.intents);
         this.intents.matches(/^version/i, this.version());
+        this.intents.onDefault(this.default());
     }
 
     version() {
         return [(session) => session.send('Ma version est 1.0 !')]
+    }
+
+    default(){
+        return [(session, args, next) => { session.send('Bonjour je suis BOT DDSIM')}];
     }
 }
