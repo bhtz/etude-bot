704c093c41263ca692a410797b2b69dd19cc1306
HEINTZ Benjamin <benjamin.heintz@soprasteria.com>
2017-04-11 16:28:15 +0200

    id dialog

--- app/bot/version.dialog.js
+++ app/bot/version.dialog.js
@@ -6,8 +6,11 @@ module.exports = class VersionDialog {
     constructor(bot) {
         this.bot = bot;
         this.intents = new builder.IntentDialog();
+        this.dataService = new DataService();
 
         this.bot.dialog('/', this.intents);
+        this.bot.dialog('/id', this.id());
+
         this.intents.matches(/^version/i, this.version());
         this.intents.onDefault(this.default());
     }
@@ -16,7 +19,18 @@ module.exports = class VersionDialog {
         return [(session) => session.send('Ma version est 1.0 !')]
     }
 
+    id(){
+        return [
+            (session) => { builder.Prompts.text(session, 'Quel est l\'ID de votre mobil\'idées ?')},
+            (session, results) => { 
+                session.id = results.response; 
+                var idea = this.dataService.get(session.id);
+                session.send(idea.shortDescription); 
+            }
+        ]
+    }
+
     default(){
-        return [(session, args, next) => { session.send('Bonjour je suis BOT DDSIM')}];
+        return [(session, args, next) => { session.send('Bonjour je suis BOT DDSIM !')}];
     }
 }
