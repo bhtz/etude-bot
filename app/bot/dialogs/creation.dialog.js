3af354c602323b5723619abddadf8c6bdf9e1520
HEINTZ Benjamin <benjamin.heintz@soprasteria.com>
2017-09-13 14:51:45 +0200

    creation link

--- app/bot/dialogs/creation.dialog.js
+++ app/bot/dialogs/creation.dialog.js
@@ -1,3 +1,6 @@
+var builder = require('botbuilder');
+var UtilsDialog = require('../utils/utils.dialog');
+
 module.exports = class CreationDialog {
 
     constructor(bot, intents) {
@@ -8,6 +11,15 @@ module.exports = class CreationDialog {
     }
 
     dialog() {
-        return [(session) => session.send('Intention matché: creation')];
+        return [(session) => {
+            session.send("Je vous invite à remplir le formulaire suivant pour renseigner votre mobil'idée");
+            var card = UtilsDialog.getLinkCard(
+                session,
+                'Portail Mobil\'idée',
+                "Nouvelle Mobil\'idée",
+                'http://mobilidees.mt.sncf.fr/#/propose'
+            );
+            session.send(card);
+        }];
     }
 }
