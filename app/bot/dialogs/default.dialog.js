df071610dd8ee9c2c68f5f5b5b0de6a00705798a
HEINTZ Benjamin <benjamin.heintz@soprasteria.com>
2017-09-19 14:38:21 +0200

    CI

--- app/bot/dialogs/default.dialog.js
+++ app/bot/dialogs/default.dialog.js
@@ -9,8 +9,7 @@ module.exports = class DefaultDialog {
 
     dialog() {
         return [(session) => { 
-            session.send(`Bonjour je suis MIA (Mobil\'Idée Assistant)! \n\n Posez moi vos questions, je me ferai une joie de vous répondre !
-            `); 
+            session.send(`Bonjour je suis MIA (Mobil\'Idée Assistant)! \n\n Posez moi vos questions, je me ferai une joie de vous répondre !`);
         }];
     }
 }
