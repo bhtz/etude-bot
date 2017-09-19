2434fedb66ee9aecd97dac78427dbb4c4b66c1ff
ext.solucom.vincent.gombert@sncf.fr <PVGT10781@COMMUN.AD.SNCF.FR>
2017-09-19 15:42:21 +0200

    improve deployment dialog

--- app/bot/dialogs/deployment.dialog.js
+++ app/bot/dialogs/deployment.dialog.js
@@ -1,3 +1,6 @@
+var builder = require('botbuilder');
+var UtilsDialog = require('../utils/utils.dialog');
+
 module.exports = class DeploymentDialog {
 
     constructor(bot, intents){
@@ -8,6 +11,32 @@ module.exports = class DeploymentDialog {
     }
 
     dialog(){
-        return [(session) => session.send('Pour déployer une application, addressez-vous au support PMM')];
+        var choices = {};
+        return [
+            (session) => {
+                builder.Prompts.text(session, 'Désirez-vous des informations sur le déploiement d\'une application ?');
+            },
+            (session, results, next) => {
+                var response = results.response.toLowerCase();
+                if (response == 'oui') {
+                    next();
+                } else if (response == 'non') {
+                    session.endDialog();
+                } else {
+                    session.send('Je n\'ai pas compris pas votre réponse.');
+                    session.endDialog();
+                }
+            },
+            (session) => {
+                session.send('Alors le plus simple est de contacter le support PMM, voici la carte de visite.');
+                var card = UtilsDialog.getLinkCard(
+                    session,
+                    'Support PMM',
+                    '*PMM-Support <<PMM-support@sncf.fr>>',
+                    'mailto://PMM-support@sncf.fr'
+                );
+                session.send(card);
+            }
+        ];
     }
 }
