d01ceb8b135dea85ecacb1ab6f596a30193dacd1
PRUNET Hélène (ext SOPRA STERIA) <ZHPT02871@COMMUN.AD.SNCF.FR>
2017-09-25 18:41:39 +0200

    Enrichissement du dialogue de création (condition si thème)

--- app/bot/dialogs/creation.dialog.js
+++ app/bot/dialogs/creation.dialog.js
@@ -12,15 +12,27 @@ module.exports = class CreationDialog {
 
     dialog() {
         return [
+            (session) => { 
+                builder.Prompts.text(session, "Tu veux ajouter une nouvelle idée, très bonne idée ! Peux tu me donner le titre que tu souhaites lui donner ?"); 
+            },
             (session, results) => {
-                session.send("Je vous invite à remplir le formulaire suivant pour renseigner votre mobil'idée");
-                var card = UtilsDialog.getLinkCard(
-                    session,
-                    'Portail Mobil\'idée',
-                    "Nouvelle Mobil\'idée",
-                    'http://mobilidees.mt.sncf.fr/#/propose'
-                );
-                session.send(card);
+                if (results.response) {
+                    session.send("Il semble que j'ai ce qui pourrait être utile.");
+                    session.send("J'ai préparé pour toi le formulaire de création de l'idée. Pour rappel, voici le thème : "+results.response);
+                    var card = UtilsDialog.getLinkCard(
+                        session,
+                        'Portail Mobil\'idée',
+                        "Nouvelle Mobil\'idée",
+                        'http://mobilidees.mt.sncf.fr/#/propose'
+                    );
+                    session.send(card);
+                    session.send("Remarque : avant d'ajouter l'idée, si besoin je peux t'aider à savoir si une idée ou une application n'existe pas déjà");
+                    session.endDialog();
+                }
+                else{
+                    session.send("Tant pis, mais n'hésite pas à revenir vers moi si tu souhaites des informations");
+                    session.endDialog();
+                }
             }
         ];
     }
